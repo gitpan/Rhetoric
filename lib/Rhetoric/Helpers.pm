85a9f9678219e6ebd4a0f2aacbf1678ab255ce81
John Beppu <beppu@cpan.org>
2011-04-27 11:44:20 +0000

    Import of BEPPU/Rhetoric-0.01 from CPAN.
    gitpan-cpan-distribution: Rhetoric
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         BEPPU/Rhetoric-0.01.tar.gz
    gitpan-cpan-author:       BEPPU
    gitpan-cpan-maturity:     released

---
+++ lib/Rhetoric/Helpers.pm
@@ -0,0 +1,35 @@
+package Rhetoric::Helpers;
+use base 'Exporter';
+use common::sense;
+use aliased 'Squatting::H';
+use Data::Page;
+use Rhetoric::Formatters;
+
+our $F = $Rhetoric::Formatters::F;
+our @EXPORT_OK   = qw(now slug $F);
+our %EXPORT_TAGS = (all => \@EXPORT_OK);
+
+# y m d h m s
+sub now {
+  my @d = localtime;
+  return (
+    $d[5]+1900,
+    $d[4]+1,
+    $d[3],
+    $d[2],
+    $d[1],
+    $d[0]
+  );
+}
+
+# make a url-friendly slug out of a post title
+sub slug {
+  # FIXME - protect against duplicate y-m-slug combinations
+  my $title = shift;
+  $title =~ s/^\W+//;
+  $title =~ s/\W+$//;
+  $title =~ s/\W+/-/g;
+  return $title;
+}
+
+1;
